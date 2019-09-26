515ab5cf4e231604a67d65833ca0453b47300bf5
a.villalobos <a.villalobos@docusign.com>
2019-09-26 13:26:57 -0500

    Revert "Adding code"
    This reverts commit 43fb70990198506ac6e192534dd1388712aad9cc.

--- reporter.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(errors) {
-    var len = errors.length,
-      filename;
-
-    if (len) {
-      filename = errors[0].file;
-      process.stdout.write(filename + '\n -- ' + len + ' hardcoded string(s)\n');
-    }
-};
